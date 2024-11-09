972a739c7357f6c2e7b3ec0059e9f22b8aa16b03
Alperen Kam <147625107+Alperenkam@users.noreply.github.com>
2024-11-10 00:47:48 +0300

    Add files via upload

---
+++ DaireAlanCevre/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace daire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dairenin yarıçapı giriniz");
+            string input = Console.ReadLine();
+            
+            int a;
+            if (int.TryParse(input, out a))
+            {
+                double cevre = 2 * Math.PI * a;
+                double alan = Math.PI * a * a;
+
+                Console.WriteLine($"Cevre {cevre:F2} Alan {alan:F2} dır");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid integer.");
+            }
+        }
+    }
+}
